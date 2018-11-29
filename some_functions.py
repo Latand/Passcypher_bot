d9fb062ae0d2e3fd3a4597e1e9f09a8a9b695b58
Latand <latand666@gmail.com>
2018-11-29 13:13:39 +0000

    Statistics fix

--- some_functions.py
+++ some_functions.py
@@ -2,13 +2,13 @@ from sql import sql
 
 
 def increase_message_counter(password=False):
-    sql.update(table="Statistics", messages="messages+1")
+    sql.update(table="Statistics", messages="`messages`+1", condition={"id": 0}, raw=True)
     if password:
-        sql.update(table="Statistics", passwords="passwords+1")
+        sql.update(table="Statistics", passwords="`passwords`+1", condition={"id": 0}, raw=True)
 
 
 def get_counters():
     users = sql.select(what="COUNT(*)", where="users")
-    messages, passwords = sql.select(where="Statistics", what=["messages", "passwords"])[0]
+    messages, passwords = sql.select(where="Statistics", what=["messages", "passwords"], condition={"id": 0})[0]
     return {"users": users, "messages": messages, "passwords": passwords}
 
