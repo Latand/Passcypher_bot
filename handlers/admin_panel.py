17cdc45bb0c7443bb2893590d7b140955b995b3a
Kostiantyn <latand666@gmail.com>
2019-05-03 14:56:49 +0100

    small fixes to admin panel

--- handlers/admin_panel.py
+++ handlers/admin_panel.py
@@ -1,8 +1,10 @@
-from asyncio import sleep
-
+from asyncio import sleep, ensure_future
+from aiogram.utils.exceptions import BotBlocked, ChatNotFound, UserDeactivated
+from google_auth import has_g_auth
 from filters import *
 from main_bot import bot, dp
 from some_functions import *
+import logging
 
 
 @dp.message_handler(IsAdmin(), commands=["send_to_all"])
@@ -10,9 +12,9 @@ async def encode_m(message: types.Message):
     increase_message_counter()
 
     text = message.text.split("/send_to_all")[1]
-    result = await sender(text)
+    result = ensure_future(sender(text))
     chat_id = message.chat.id
-    await message.reply(chat_id, "Готово. Ошибки: {}".format(result))
+    await bot.send_message(chat_id, "Готово. Ошибки: {}".format(result))
 
 
 async def sender(message: str):
@@ -20,8 +22,15 @@ async def sender(message: str):
     errors = set()
     for (user,) in users_list:
         try:
-            await bot.send_message(chat_id=user, message=message)
+            await bot.send_message(chat_id=user, text=message)
+        except (BotBlocked, ChatNotFound, UserDeactivated):
+            if not has_g_auth(user):
+                try:
+                    sql.delete(table="users", where=["chat_id"], what=user)
+                except Exception as error:
+                    logging.error(error)
         except Exception as err:
+            logging.error(err)
             errors.add(err)
         finally:
             await sleep(1)
