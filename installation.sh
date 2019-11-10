b16dd91488f0ae3ab20dbc1e8740d671d5cc409e
Kostiantyn Kriuchkov <36363097+Latand@users.noreply.github.com>
2019-11-10 20:17:23 +0200

    added languages compilation

--- installation.sh
+++ installation.sh
@@ -11,5 +11,13 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-get update && sudo apt-get install docker-ce -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
 mv bot/utils/secret.example.py bot/utils/secret.py
+
+echo "Compiling languages..."
+pybabel extract bot/ -o locales/pcypher.pot
+pybabel init -i locales/pcypher.pot -d locales -D pcypher -l en
+pybabel init -i locales/pcypher.pot -d locales -D pcypher -l ru
+pybabel init -i locales/pcypher.pot -d locales -D pcypher -l uk
+pybabel compile -d locales -D pcypher
+
 echo "Starting bot..."
 sudo docker-compose up
